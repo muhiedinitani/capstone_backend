de2c7ec9b4f42cec7cf30f579244768a6e3f56cb
Dean Itani <muhiedin.itani@gmail.com>
2021-12-02 19:25:20 -0600

    add create action with validations and add 3 users to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_004634) do
+ActiveRecord::Schema.define(version: 2021_12_03_010940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
