d85b163b27be54fe303a6d392b8f2a64b4bb9252
Dean Itani <muhiedin.itani@gmail.com>
2021-12-02 20:31:43 -0600

    add trips controller + add create trip action + add associations for user/trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,14 @@
 class TripsController < ApplicationController
+  trip = Trip.new(
+    resort_id: params[:resort_id],
+    day_start: params[:day_start],
+    day_end: params[:day_end],
+    user_id: params[:user_id],
+    original_query: params[:original_query]
+  )
+  if trip.save
+    render json: { message: "Trip created successfully" }, status: :created
+  else
+    render json: { errors: trip.errors.full_messages }, status: :bad_request
+  end
 end
