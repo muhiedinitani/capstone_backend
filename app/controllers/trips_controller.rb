c80e326828e782564c276c3b1d4ca9a328fe82cc
Dean Itani <muhiedin.itani@gmail.com>
2021-12-09 19:34:48 -0600

    modify trip controller + jwt to requests file

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,14 +1,19 @@
 class TripsController < ApplicationController
-  trip = Trip.new(
-    resort_id: params[:resort_id],
-    day_start: params[:day_start],
-    day_end: params[:day_end],
-    user_id: params[:user_id],
-    original_query: params[:original_query]
-  )
-  if trip.save
-    render json: { message: "Trip created successfully" }, status: :created
-  else
-    render json: { errors: trip.errors.full_messages }, status: :bad_request
+  before_action :authenticate_user, only: [:create] #you can add more methods if needed. add a comma after :create
+
+  def create
+    trip = Trip.new(
+      resort_id: params[:resort_id],
+      day_start: params[:day_start],
+      day_end: params[:day_end],
+      user_id: current_user.id,
+      original_query: params[:original_query]
+    )
+    if trip.save
+      render json: { message: "Trip created successfully and creator is #{current_user.id}" }, status: :created
+    else
+      render json: { errors: trip.errors.full_messages }, status: :bad_request
+    end
   end
+
 end
