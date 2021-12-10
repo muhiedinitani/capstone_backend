58149efd3fce47f25acc6c3cdbbbfbea1a00c3fc
Dean Itani <muhiedin.itani@gmail.com>
2021-12-09 20:53:54 -0600

    migrate to include resorts table in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_021448) do
+ActiveRecord::Schema.define(version: 2021_12_10_025143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "resorts", force: :cascade do |t|
+    t.string "name"
+    t.string "official_website"
+    t.float "lat"
+    t.float "lng"
+    t.integer "region_id"
+    t.string "region_name"
+    t.integer "original_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.integer "resort_id"
