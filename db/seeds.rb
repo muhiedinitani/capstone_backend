976ebb4f7362a6497e8535af88702cd8e108046a
Dean Itani <muhiedin.itani@gmail.com>
2021-12-12 10:50:35 -0600

    added multiple resorts, added multiples users, backend for now compelte, beginning frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 skimap_index = JSON.parse(File.read(Rails.root.join('db/skimapindex.json')))
-skimap = skimap_index["skiAreas"]["skiArea"][0]
+skimap = skimap_index["skiAreas"]["skiArea"]
 
-x = Resort.new(
-  name: skimap["name"]["__cdata"], # name; working
-  official_website: skimap["officialWebsite"]["__cdata"], # website; working
-  lat: skimap["georeferencing"]["_lat"].to_f, #lat; working
-  lng: skimap["georeferencing"]["_lng"].to_f, #lng; working
-  region_id: skimap["regions"]["region"][0]["_id"].to_i, # region_id; working
-  region_name: skimap["regions"]["region"][0]["__cdata"], #region_name; working
-  original_id: skimap["_id"].to_i #original_id; working
-)
+skimap.each do |i|
+  resort = Resort.new(
+    name: i["name"]["__cdata"], # name; working
+    official_website: i["officialWebsite"] && i["officialWebsite"]["__cdata"], # website; working
+    lat: i["georeferencing"]["_lat"].to_f, #lat; working
+    lng: i["georeferencing"]["_lng"].to_f, #lng; working
+    region_id: i["regions"]["region"][0]["_id"].to_i, # region_id; working
+    region_name: i["regions"]["region"][0]["__cdata"], #region_name; working
+    original_id: i["_id"].to_i #original_id; working
+  )
+  resort.save
+end
 
-x.save
-
-#continue here, you got it to work on one resort. just have it go thru the entire json with an .each do. need to speed things up. once you have your db of resorts, your backend is pretty much complete. no need to have a resort create for time being. 
-
-
-
-
-# skimap.each do |resort|
-#   Resort.new(
-#     name: resort["name"]["__cdata"], # name; working
-#     offical_website: resort["officialWebsite"]["__cdata"], # website; working
-#     lat: resort["georeferencing"]["_lat"].to_f, #lat; working
-#     lng: resort["georeferencing"]["_lng"].to_f, #lng; working
-#     region_id: resort["regions"]["region"][0]["_id"].to_i, # region_id; working
-#     region_name: resort["regions"]["region"][0]["__cdata"], #region_name; working
-#     original_id: resort["_id"].to_i #original_id; working
-#   )
-#   Resort.save
-# end
+# fix this later with resorts that aren't migrating
