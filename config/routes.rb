6346eec23fdafd9e3e215250450d762fda1282d6
Dean Itani <muhiedin.itani@gmail.com>
2021-12-14 19:29:33 -0600

    add user show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
   post "/sessions" => "sessions#create"
   get "/trips" => "trips#index"
   post "/trips" => "trips#create"
-  post "/trips/:id" => "trips#show"
+  get "/trips/:id" => "trips#show"
   get "/resorts" => "resorts#index"
   get "/resorts/:id" => "resorts#show"
   get "/nearby_resorts" => "resorts#nearby_resorts"
